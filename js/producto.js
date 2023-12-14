c67596ee36401fd5a1ab6d08b98420c20ca36583
Camila <camilab@duck.com>
2023-12-14 13:01:58 +0100

    uno

---
+++ js/producto.js
@@ -0,0 +1,11 @@
+class Producto {
+    nombre;
+    precio;
+    talle;
+  
+    constructor(nombre, precio, talle) {
+      this.nombre = nombre;
+      this.precio = precio;
+      this.talle = talle;
+    }
+  }
